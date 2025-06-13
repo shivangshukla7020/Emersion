cb5318bdcb3f8b7f0309efaaf72aaa9b164115e2
shivangshukla7020 <shivangshukla201@gmail.com>
2025-06-13 09:57:25 +0530

    [Shivang] Add : Q26-SwapTwoNumbers.java

---
+++ Java Programming Elements/SwapTwoNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class SwapTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+
+        int temp = number1;
+        number1 = number2;
+        number2 = temp;
+
+        System.out.println("The swapped numbers are " + number1 + " and " + number2);
+    }
+}
