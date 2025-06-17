8fc402d733c67ab5878ec11b94f3ca15d246dd41
shivangshukla7020 <shivangshukla201@gmail.com>
2025-06-17 09:01:43 +0530

    [Shivang] Add : GreatestFactor.java

--- Control Flows/GreatestFactor.java
+++ Control Flows/GreatestFactor.java
@@ -1,20 +1,12 @@
-import java.util.Scanner;
-
-public class GreatestFactor {
+import java.util.*;
+public class Q27 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        int greatestFactor = 1;
-        int counter = number - 1;
-
-        while (counter >= 1) {
-            if (number % counter == 0) {
-                greatestFactor = counter;
+        int n = new Scanner(System.in).nextInt();
+        for (int i = n - 1; i >= 1; i--) {
+            if (n % i == 0) {
+                System.out.println(i);
                 break;
             }
-            counter--;
         }
-
-        System.out.println("Greatest factor of " + number + " is: " + greatestFactor);
     }
 }
