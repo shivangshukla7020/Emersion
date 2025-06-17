a3f8c24605654737e818c77b9224f9919d561dbf
shivangshukla7020 <shivangshukla201@gmail.com>
2025-06-17 09:00:41 +0530

    fixed mismatch

--- Java Programming Elements/LeapYearCheck.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Q30 {
-    public static void main(String[] args) {
-        int y = new Scanner(System.in).nextInt();
-        if (y < 1582) return;
-        System.out.println((y % 4 == 0 && y % 100 != 0) || y % 400 == 0 ? "Leap Year" : "Not a Leap Year");
-    }
-}
